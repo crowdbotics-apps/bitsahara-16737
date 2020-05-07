fd19c9d9fb679f329ff5343b0b96000d7a4786ce
Crowdbotics <team@crowdbotics.com>
2020-05-07 22:51:36 +0000

    Update models

--- bitsahara_16737/wsgi.py
+++ bitsahara_16737/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitsahara_16737.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bitsahara_16737.settings")
 
 application = get_wsgi_application()
